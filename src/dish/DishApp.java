53447b720e88ffefce674e610ffdf029c304b9d1
YIMI <2798576491@qq.com>
2024-09-09 14:31:54 +0800

    YIMI

--- src/dish/DishApp.java
+++ src/dish/DishApp.java
@@ -1,10 +1,27 @@
 package dish;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //点菜
 public class DishApp {
     //提前准备菜单
+    //定义一个集合表示菜品
+    static List<Dish> dishList=new ArrayList<>();
     public static void main(String[] args) {
+        initDish();
+        System.out.println(dishList);
+    }
+    //初始化菜品
+    public static void initDish(){
         Dish dish=new Dish(1,"香辣肉丝",29.0);
+        dishList.add(dish);
         Dish dish2=new Dish(2,"炖王八",59.0);
+        dishList.add(dish2);
+
+        //存放菜品
+        dishList.add(new Dish(3,"凉菜",21.0));
+        dishList.add(new Dish(4,"三县",24.0));
+        dishList.add(new Dish(5,"大虾",55.0));
     }
 }
