d01be1972302630914fe7fe5d7253773dfc7abf7
YIMI <2798576491@qq.com>
2024-09-09 14:17:08 +0800

    YIMI

---
+++ src/dish/DishApp.java
@@ -0,0 +1,10 @@
+package dish;
+
+//点菜
+public class DishApp {
+    //提前准备菜单
+    public static void main(String[] args) {
+        Dish dish=new Dish(1,"香辣肉丝",29.0);
+        Dish dish2=new Dish(2,"炖王八",59.0);
+    }
+}
